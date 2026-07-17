6d5f81f924904ccf002296a11639a134f3671a9f
bharat suthar <bharatsuthar1632@gmail.com>
2026-07-17 18:43:26 +0530

    Fix private key formatting in Firebase admin initialization

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -10,7 +10,7 @@ const adminApp =
       credential: cert({
         projectId:   process.env.FIREBASE_ADMIN_PROJECT_ID!,
         clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL!,
-        privateKey:  process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey:  process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/^"|"$/g, '').replace(/\\n/g, '\n'),
       }),
       databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
     },
